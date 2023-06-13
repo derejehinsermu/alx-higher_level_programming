b0629834f39d4094f20de35a154f9a903d3a51d1
derejehinsermu <derejehinsermu2@gmail.com>
2023-06-12 17:58:04 -0700

    cpython

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,5 @@
 if __name__ == "__main__":
     import sys
     
-    total = 0
-    for arg in sys.argv[1:]:
-        total += int(arg)
+    total = sum(int(arg) for arg in sys.argv[1:])
     print("{}".format(total))
