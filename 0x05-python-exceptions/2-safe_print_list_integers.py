401912f2b721df0683e12c56955159127e7ca010
derejehinsermu <derejehinsermu2@gmail.com>
2023-06-26 16:37:18 -0700

    print list of integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for item in my_list:
+            if isinstance(item, int):
+                print("{:d}".format(item), end = "")
+                count += 1
+                if count >= x:
+                    break
+        print()
+    except (TypeError, IndexError):
+        print("error occured")
+
+    return (count)
