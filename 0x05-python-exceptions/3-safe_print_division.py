e51126aaaa194061396930b0f2ba182a94f30f4e
derejehinsermu <derejehinsermu2@gmail.com>
2023-06-26 16:59:13 -0700

    safe print division integer

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,6 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside Result: {}".format(result))
+        print("Inside result: {}".format(result))
     return (result)
 
