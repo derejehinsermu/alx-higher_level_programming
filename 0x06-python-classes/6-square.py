93bd6d38c168e23a2d831a292fcc7fd22810a958
derejehinsermu <derejehinsermu2@gmail.com>
2023-06-28 11:14:29 -0700

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""Define a class called A Square"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+        __position (tuple): The position of the square (private).
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes a Square instance with an optional size and position.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+            position (tuple, optional): The position of the square. Defaults to (0, 0).
+
+        Raises:
+            TypeError: If size is not an integer or position is not a tuple of 2 positive integers.
+            ValueError: If size is less than 0 or position contains non-positive integers.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """
+        Getter method to retrieve the position of the square.
+
+        Returns:
+            tuple: The position of the square.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Setter method to set the position of the square.
+
+        Args:
+            value (tuple): The position of the square.
+
+        Raises:
+            TypeError: If position is not a tuple of 2 positive integers.
+            ValueError: If position contains non-positive integers.
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """
+        Calculates and returns the current square area.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints the square with the character # and the given position.
+
+        If size is equal to 0, it prints an empty line.
+        The position is used to determine the indentation of each line.
+        """
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
