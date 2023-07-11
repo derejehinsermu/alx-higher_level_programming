f25a67259351354f2cf49ad1e4ec776f30e4d171
derejehinsermu <derejehinsermu2@gmail.com>
2023-07-11 16:13:18 -0700

    add all argument

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 
 
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
 
 try:
