ee9579377f7f94a8d7d8f559c668efbbf3b9746c
derejehinsermu <derejehinsermu2@gmail.com>
2023-07-11 15:45:30 -0700

    json_file

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/urs/bin/python3
+""" define a function called to_json_string"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be converted to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
+
