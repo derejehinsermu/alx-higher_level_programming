61da3fc59b596b589470bba1b362e3ffe3bcc275
derejehinsermu <derejehinsermu2@gmail.com>
2023-07-12 16:17:23 -0700

    mplementation of the BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""mplementation of the BaseGeometry class """
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        Calculates the area of the geometry.
+
+        Raises:
+            Exception: This method is not implemented.
+
+        Returns:
+            None
+        """
+        raise Exception("area() is not implemented")
