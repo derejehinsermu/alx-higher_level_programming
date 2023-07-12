deb6bcc15076252520749d7176ea3590f9732fdd
derejehinsermu <derejehinsermu2@gmail.com>
2023-07-12 15:23:58 -0700

    def lookup(obj):

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" function that returns the list of available attributes and methods of an object:"""
+
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: List of available attributes and methods of the object.
+    """
+    return [attr for attr in dir(obj) if not attr.startswith("__")]
