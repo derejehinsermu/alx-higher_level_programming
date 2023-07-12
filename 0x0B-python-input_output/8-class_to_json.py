20a24dd0c255952fc3045218179a1aedd101cd2e
derejehinsermu <derejehinsermu2@gmail.com>
2023-07-11 17:42:16 -0700

    class_to_json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -13,10 +13,12 @@ def class_to_json(obj):
         dict: The dictionary representation of the object suitable for JSON serialization.
     """
     json_dict = {}
+
     for attr_name, attr_value in obj.__dict__.items():
         if isinstance(attr_value, (list, dict, str, int, bool)):
             json_dict[attr_name] = attr_value
         elif hasattr(attr_value, '__dict__'):
             json_dict[attr_name] = class_to_json(attr_value)
+
 
     return json_dict
