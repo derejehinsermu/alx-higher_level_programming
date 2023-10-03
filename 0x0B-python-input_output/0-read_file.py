7c80f531055c6dd96f5c2e8ed7dbf65e6459724b
Dereje Hinsermu Senbatu <45657872+derejehinsermu@users.noreply.github.com>
2023-10-02 17:15:18 -0700

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Define the function clalled read_file """
-
-
-def read_file(filename=""):
-    """Prints the contents of a UTF8 text file"""
-    with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
